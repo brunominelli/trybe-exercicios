b59d7acde5417fc4789f329b21cbc8baaad316ac
Bruno Minelli <minellibruno@hotmail.com>
2021-11-29 20:18:46 -0300

    Envio dos exercícios do dia 04 bloco 04 incompletos.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/funcoes-matematicas.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/funcoes-matematicas.js
@@ -6,6 +6,7 @@
 // Contantes das funções
 const a = 9;
 const b = 3;
+const c = 7
 
 // Função: Adição
 function adicao(a,b){
@@ -33,17 +34,47 @@ function modulo(a,b){
 }
 
 /* Resultados */
-// Soma
+// Exercício 01.01 - Adição
+console.log('Exercício 01.01 - Adição');
 console.log(a + ' + ' + b + ' = ' + adicao(a,b));
 
-// Subtração
+// Exercício 01.02 - Subtração
+console.log('\nExercício 01.02 - Subtração');
 console.log(a + ' - ' + b + ' = ' + subtracao(a,b));
 
-// Multplicação
+// Exercício 01.03 - Multplicação
+console.log('\nExercício 01.03 - Multiplicação');
 console.log(a + ' * ' + b + ' = ' + multiplicacao(a,b));
 
-// Divisão
+// Exercício 01.04 - Divisão
+console.log('\nExercício 01.04 - Divisão');
 console.log(a + ' / ' + b + ' = ' + divisao(a,b));
 
-// Módulo
+// Exercício 01.05 - Módulo
 console.log(a + ' % ' + b + ' = ' + (modulo(a,b)));
+
+// Exercício 02
+console.log('\nExercício 02 - Maior de Dois Números');
+function maiorDeDoisNumeros(a,b){
+    if (a > b) {
+        return a + ' é maior do que ' + b;
+    } else if (a < b) {
+        return b + ' é maior do que ' + a;
+    }
+}
+console.log(maiorDeDoisNumeros(a,b));
+
+// Exercício 03
+console.log('\nExercício 03 - Maior de Três Números');
+function maiorDeTresNumeros(a,b,c){
+    if (a > b && a > c) {
+        return a + ' é maior do que ' + b + ' e ' + c;
+    } else if (b > a && b > c) {
+        return b + ' é maior do que ' + a + ' e ' + c;
+    } else if (c > a && c > b) {
+        return c + ' é maior do que ' + a + ' e ' + b;
+    }
+}
+console.log(maiorDeTresNumeros(a,b,c));
+
+// Exercício 04
