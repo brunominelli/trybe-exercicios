1a65862a9f773ce8b8cf40abcdb0863105478ebe
Bruno Minelli <minellibruno@hotmail.com>
2021-11-30 17:50:27 -0300

    Envio dos exercícios do dia 04 bloco 04 partes 1 e 2.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios.js
@@ -1,4 +1,5 @@
 /* EXERCÍCIOS PARTE 1 - OBJETOS E FOR/IN */
+console.log('\nEXERCÍCIOS PARTE 1 - OBJETOS E FOR/IN');
 let key;
 let info = {
     personagem: 'Margarida',
@@ -68,3 +69,127 @@ let leitor = {
 
 // Exercício 08
 console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.');
+
+/* EXERCÍCIOS PARTE 2 - FUNÇÕES */
+// Exercício 01
+console.log('\nEXERCÍCIOS PARTE 2 - FUNÇÕES');
+
+console.log('\nExercício 01 - Verifica Palíndromo');
+
+function verificaPalindromo(palavra) {
+  for (let index in palavra) {
+    if (palavra[index] != palavra[(palavra.length - 1) - index]) {
+      return false;
+    } else return true;
+  }
+}
+
+console.log('Arara: ' + verificaPalindromo('arara'));
+console.log('Desenvolvimento: ' + verificaPalindromo('desenvolvimento'));
+
+// Exercício 02
+console.log('\nExercício 02 - Maior Índice');
+
+const arrayMaiorIndice = [2, 3, 6, 7, 10, 1];
+
+function maiorIndice (array) {
+  let maiorIndice = 0;
+  for(let index in array){
+    if(array[maiorIndice] < array[index]) maiorIndice = index;
+  }
+  return maiorIndice;
+}
+console.log('Lista de numeros: ' + arrayMaiorIndice)
+console.log('Maior índice: ' + maiorIndice(arrayMaiorIndice));
+
+// Exercício 03
+console.log('\nExercício 03 - Menor Índice');
+
+const arrayMenorIndice = [2, 4, 6, 7, 10, 0, -3];
+
+function menorIndice(array) {
+  let menorIndice = 0;
+  for(let index in array){
+    if(array[menorIndice] > array[index]) menorIndice = index;
+  }
+  return menorIndice;
+}
+console.log('Lista de Numeros: '+ arrayMenorIndice);
+console.log('Menor indice: ' + menorIndice(arrayMenorIndice));
+
+// Exercício 04
+console.log('\nExercício 04 - Maior Nome');
+
+const arrayNomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+function maiorNome(array) {
+  let maiorNome = array[0];
+  for(let index in array){
+    if(maiorNome.length < array[index].length) maiorNome = array[index];
+  }
+  return maiorNome;
+}
+console.log('Lista de nomes: ' + arrayNomes);
+console.log('Maior nome: ' + maiorNome(arrayNomes));
+
+// Exercício 05
+console.log('\nExercício 05 - Maior Ocorrência');
+
+const arrayMaiorOcorrencia = [2, 3, 2, 5, 8, 2, 3];
+
+function verificarMaiorOcorrencia(array){
+  let contOcorrencia = 0;
+  let contValor = 0;
+  let indexValor = 0;
+
+  for(let firstIndex in array){
+    let verificarOcorrencia = array[firstIndex];
+    for(let secondIndex in array) {
+      if (verificarOcorrencia === array[secondIndex]) contValor++;
+    }
+    if (contValor > contOcorrencia) {
+      contOcorrencia = contValor;
+      indexValor = firstIndex;
+    }
+    contValor = 0;
+  }
+  return array[indexValor];
+}
+
+console.log('Lista de números: ' + arrayMaiorOcorrencia);
+console.log('Maior Ocorrência: ' + verificarMaiorOcorrencia(arrayMaiorOcorrencia));
+
+// Exercício 06
+console.log('\nExercício 06 - Somatória');
+
+const n = 5;
+
+function somatoria(n){
+  let resultado = 0;
+  for(let i = n; i > 0; i--){
+    resultado += i;
+  }
+  return resultado
+}
+
+console.log('A somatória de ' + n + ' é ' + somatoria(n));
+
+// Exercício 07
+console.log('\nExercício 07 - É o final da palavra?');
+
+const word = 'trybe';
+const ending = 'be';
+
+function verificaFinalDaPalavra(word, ending){
+  word = word.split('');
+  ending = ending.split('');
+  let status = true;
+  
+  for(let i = 0; i < ending.length; i++){
+    if(word[word.length - ending.length + i] != ending[i]) status = false;
+  }
+  return status;
+}
+
+console.log('Palavra: ' + word + ' | Final da palavra: ' + ending);
+console.log('Resultado: ' + verificaFinalDaPalavra(word,ending));
