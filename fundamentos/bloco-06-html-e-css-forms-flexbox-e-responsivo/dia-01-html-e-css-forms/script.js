84a7348ebbb281dbbcc9cde111dce83f0b7f83ed
Bruno Minelli <minellibruno@hotmail.com>
2021-12-27 12:22:01 -0300

    Identação HTML | Cometários de Funções JS | Ajuste de layout CSS

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -1,41 +1,43 @@
-const elementInitialDate = document.getElementById('data-inicio');
-const elementSubmitButton = document.getElementById('botao-enviar');
-
 /* Função: setElementOption
 -- Iunclui todos os estados do Brasil na tag Select */
 function setStates() {
-    const elementState = document.getElementById('state');
-    const arrayStates = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+  const elementState = document.getElementById('state');
+  const arrayStates = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
 
-    for (let index = 0; index < arrayStates.length; index += 1) {
-        const elementOption = document.createElement('option');
-        elementOption.innerText = arrayStates[index];
-        elementOption.value = arrayStates[index].toLowerCase();
-        elementState.appendChild(elementOption);
-    }
+  for (let index = 0; index < arrayStates.length; index += 1) {
+    const elementOption = document.createElement('option');
+    elementOption.innerText = arrayStates[index];
+    elementOption.value = arrayStates[index].toLowerCase();
+    elementState.appendChild(elementOption);
+  }
 }
 
+/* Função: clearForm
+-- Limpa todos os campos do formulário */
 function clearForm() {
-    const formElements = document.getElementsByTagName('input');
-    const elementTextarea = document.getElementsByTagName('textarea');
+  const formElements = document.getElementsByTagName('input');
+  const elementTextarea = document.getElementsByTagName('textarea');
 
-    for (let index = 0; index < formElements.length; index += 1) {
-        let data = formElements[index];
-        data.value = '';
-        elementTextarea.value = '';
-    }
+  for (let index = 0; index < formElements.length; index += 1) {
+    let data = formElements[index];
+    data.value = '';
+    elementTextarea.value = '';
+  }
 }
 
+/* Função: checkForm
+-- Impede o comportamento padrão do formulário por meio do botão de submissão do formulário */
 function checkForm(event) {
-    event.preventDefault();
+  event.preventDefault();
 }
 
+/* Execução do script apó o carregamento da página. */
 window.onload = () => {
-    setStates();
-    
-    const buttonClear = document.getElementById('button-clear');
-    buttonClear.addEventListener('click', clearForm);
+  setStates();
+  
+  const buttonClear = document.getElementById('button-clear');
+  buttonClear.addEventListener('click', clearForm);
 
-    const buttonSubmit = document.getElementById('button-submit');
-    buttonSubmit.addEventListener('click', checkForm);
+  const buttonSubmit = document.getElementById('button-submit');
+  buttonSubmit.addEventListener('click', checkForm);
 }
