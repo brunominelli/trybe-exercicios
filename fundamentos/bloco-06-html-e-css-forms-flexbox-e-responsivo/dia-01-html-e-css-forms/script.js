802e9dc8fd04b36e704db0c0b02e9c15258c0358
Bruno Minelli <minellibruno@hotmail.com>
2021-12-27 17:30:13 -0300

    Validação do formulário concluída.

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -1,8 +1,10 @@
+let message = '';
+let fieldsName = [];
 /* Função: setElementOption
 -- Iunclui todos os estados do Brasil na tag Select */
 function setStates() {
   const elementState = document.getElementById('state');
-  const arrayStates = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+  const arrayStates = ['', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
 
   for (let index = 0; index < arrayStates.length; index += 1) {
     const elementOption = document.createElement('option');
@@ -12,43 +14,76 @@ function setStates() {
   }
 }
 
+function validadteState() {
+    const elementState = document.getElementById('state');
+    if (elementState.value === '') {
+        fieldsName.push(elementState.name.toUpperCase());
+        elementState.style.borderColor = 'red'
+    } else elementState.style.borderColor = 'green';
+}
 
-function validateFormFields() {
+/* Função: validateDate
+-- Verifica se a data inserida pela pessoa usuária está correta */
+function validateDate() {
+  const elementStartDate = document.getElementById('start-date');
+  const arrayDate = elementStartDate.value.split('/');
+  const day = parseInt(arrayDate[0]);
+  const month = parseInt(arrayDate[1]);
+  const year = parseInt(arrayDate[2]);
+  let color = 'green';
+
+  if (day <= 0 || day > 31){
+    alert('Valor inválido: Dia incorreto!');
+    color = 'red';
+    elementStartDate.style.borderColor = color;
+  }
+
+  if (month <= 0 || month > 12) {
+    alert('Valor inválido: Mês incorreto!');
+    color = 'red';
+    elementStartDate.style.borderColor = color;
+  }
+
+  if (year < 0) {
+    alert('Valor inválido: Ano incorreto!');
+  }
+}
+
+/* Função: validateInputFormFields
+-- Verifica se os campos de texto do formulário foram preenchidos pela pessoa usuária */
+function validateInputFormFields() {
   const elementsInput = document.getElementsByTagName('input');
   const elementsTextarea = document.getElementsByTagName('textarea')[0];
-  const elementsLabel = document.getElementsByTagName('label');
-  let color = 'black';
-  let message = 'Preencha todos os campos obrigatórios!';
-
+  let arrayInputText = [];
   for (let index = 0; index < elementsInput.length; index += 1) {
-    // console.log(elementsInput[index]);
-    // console.log(elementsLabel[10]);
-
     if (elementsInput[index].type === 'text') {
-      if (elementsInput[index].value === '') {
-        color = 'red';
-        elementsInput[index].style.borderColor = color;
-      } else if (elementsInput[index].value !== '') {
-        elementsInput[index].style.borderColor = color;
-      }
-    }
-
-    if (elementsInput[index].type === 'email') {
-      color = 'red';
-      elementsInput[index].style.borderColor = color;
-    } else if (elementsInput[index].value !== '') {
-      elementsInput[index].style.borderColor = color;
-    }
-
-    if (elementsTextarea.value === '') {
-        color = 'red';
-        elementsTextarea.style.borderColor = color;
-    } else if (elementsTextarea.value !== '') {
-        elementsTextarea.style.borderColor = color;
+      arrayInputText.push(elementsInput[index]);
     }
   }
-  
-  return alert(message);
+
+  for (let index = 0; index < arrayInputText.length; index += 1) {
+    if (arrayInputText[index].value === '') {
+      fieldsName.push(arrayInputText[index].name.toUpperCase());
+      arrayInputText[index].style.borderColor = 'red';
+    } else arrayInputText[index].style.borderColor = 'green';
+  }
+
+  if (elementsTextarea.value === '') {
+    fieldsName.push(elementsTextarea.name.toUpperCase());
+    elementsTextarea.style.borderColor = 'red';
+  } else {
+    elementsTextarea.style.borderColor = 'green';
+  }
+}
+
+/* Função: validateFormFields
+-- Verifica se os campos do formulário foram preenchidos */
+function validateEmail() {
+  const elementInputEmail = document.getElementById('email');
+  if (elementInputEmail.value === '' || (elementInputEmail.value.indexOf('@') === -1)) {
+    fieldsName.push(elementInputEmail.name.toUpperCase());
+    elementInputEmail.style.borderColor = 'red';
+  } else elementInputEmail.style.borderColor = 'green';
 }
 
 /* Função: clearForm
@@ -68,8 +103,18 @@ function clearForm() {
 -- Impede o comportamento padrão do formulário por meio do botão de submissão do formulário */
 function checkForm(event) {
   event.preventDefault();
-  validateFormFields(event);
+  validateInputFormFields();
+  validadteState();
+  validateEmail();
+  validateDate();
 
+  if (fieldsName.length !== 0) {
+      message = `Preencha todos os campos obrigatórios! ${fieldsName}`; 
+      alert(message);
+  }
+
+  message = '';
+  fieldsName = [];
 }
 
 /* Execução do script apó o carregamento da página. */
