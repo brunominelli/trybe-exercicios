ca7e22bed97eea210a1e8f0773a8b4c08b035944
Bruno Minelli <minellibruno@hotmail.com>
2022-01-18 17:14:19 -0300

    Exercicio 03 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicios.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicios.js
@@ -1,5 +1,5 @@
 // Exercício 01
-console.log('Exercício 01')
+console.log('Exercício 01');
 const newEmployees = (callback) => {
   const employees = {
    id1: createEmployss('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
@@ -19,7 +19,7 @@ const createEmployss = (fullName) => (
 console.log(newEmployees(createEmployss));
 
 // Exercício 02
-console.log('\nExercício 02')
+console.log('\nExercício 02');
 const raffle = (bet, callback) => {
   const randomNumber = Math.floor(Math.random() * 5) + 1;
   return ` Aposta: ${bet}\n Resultado: ${randomNumber}\n ${!callback(bet, randomNumber) ? 'Tente novamente' : 'Parabéns você ganhou'}`;
@@ -27,3 +27,26 @@ const raffle = (bet, callback) => {
 const raffleResult = (bet, raffleNumber) => bet === raffleNumber;
 
 console.log(raffle(3, raffleResult));
+
+// Exercício 03
+console.log('\nExercício 03');
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const validateAnswers = (rightAnswers, studentAnswers) => {
+    if (rightAnswers === studentAnswers) return 1;
+    if (studentAnswers === 'N.A') return 0;
+    return 0.5;
+}
+
+const evaluation = (rightAnswers, studentAnswers, callback) => {
+    let score = 0;
+    for (let index = 0; index < rightAnswers.length; index += 1) {
+        const result = callback(rightAnswers[index], studentAnswers[index]);
+        score += result;
+    }
+    return `Nota: ${score}`
+};
+
+console.log(evaluation(RIGHT_ANSWERS, STUDENT_ANSWERS, validateAnswers));
