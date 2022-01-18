683bf5f5ea0dd85b14f0f345792a5ee5642f695d
Bruno Minelli <minellibruno@hotmail.com>
2022-01-18 16:54:30 -0300

    Exercicio 02 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicios.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicios.js
@@ -1,3 +1,5 @@
+// Exercício 01
+console.log('Exercício 01')
 const newEmployees = (callback) => {
   const employees = {
    id1: createEmployss('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
@@ -15,3 +17,13 @@ const createEmployss = (fullName) => (
 );
 
 console.log(newEmployees(createEmployss));
+
+// Exercício 02
+console.log('\nExercício 02')
+const raffle = (bet, callback) => {
+  const randomNumber = Math.floor(Math.random() * 5) + 1;
+  return ` Aposta: ${bet}\n Resultado: ${randomNumber}\n ${!callback(bet, randomNumber) ? 'Tente novamente' : 'Parabéns você ganhou'}`;
+} 
+const raffleResult = (bet, raffleNumber) => bet === raffleNumber;
+
+console.log(raffle(3, raffleResult));
