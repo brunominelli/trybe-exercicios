95432873f993836bb55b21fd39f5a9fc8738a3b2
Bruno Minelli <minellibruno@hotmail.com>
2022-01-19 18:38:35 -0300

    Exercicio 06 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-higher-order-funcitons-foreach-find-some-every-sort/exercicio06.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-higher-order-funcitons-foreach-find-some-every-sort/exercicio06.js
@@ -62,3 +62,11 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+  const expectedResult = true;
+
+function someBookWasReleaseOnThe80s() {
+  // escreva seu código aqui
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear < 1990);
+}
+
+console.log(someBookWasReleaseOnThe80s());
