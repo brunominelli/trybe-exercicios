73f943aaa6b6093bd2151c474c0c72e5c508cf5b
Bruno Minelli <minellibruno@hotmail.com>
2022-01-20 17:16:55 -0300

    Exercicio 07 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-map-e-filter/exercicio7.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-map-e-filter/exercicio7.js
@@ -62,3 +62,14 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+  const expectedResult = 'O Senhor dos Anéis';
+
+  function authorWith3DotsOnName() {
+    // escreva seu código aqui
+    return books
+      .find((book) => book.author.name.split(' ')
+      .filter((word) => word
+      .endsWith('.')).length === 3).name;
+  };
+
+  console.log(authorWith3DotsOnName());
