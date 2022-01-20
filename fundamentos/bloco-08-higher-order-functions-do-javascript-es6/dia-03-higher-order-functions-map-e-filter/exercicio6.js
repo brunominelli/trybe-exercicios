99ff7bae769a3834228f0ce6c8d981764ccf6f58
Bruno Minelli <minellibruno@hotmail.com>
2022-01-20 17:06:12 -0300

    Exercicio 06 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-map-e-filter/exercicio6.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-map-e-filter/exercicio6.js
@@ -62,3 +62,18 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+  const expectedResult = [
+    'O Senhor dos Anéis',
+    'Fundação',
+    'O Chamado de Cthulhu',
+  ];
+  
+  function oldBooks() {
+    // escreva seu código aqui
+    const currentYear = new Date().getFullYear();
+    return books
+      .filter((book) => currentYear - book.releaseYear > 60)
+      .map((book) => book.name);
+  };
+
+  console.log(oldBooks());
